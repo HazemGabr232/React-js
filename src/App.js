6b0ed9c1b8d638bf780ec56b85f507a8f5405b10
hazemgabr232 <hazemgabr232@gmail.com>
2020-04-01 03:40:45 +0200

    update HomeComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Menu from "./components/MenuComponent";
 import "./App.css";
 import { DISHES } from "./shared/dishes";
+import { COMMENTS } from "./shared/comments";
+import { LEADERS } from "./shared/leaders";
+import { PROMOTIONS } from "./shared/promotions";
 import Header from "./components/HeaderComponent";
 import Footer from "./components/FooterComponent";
 import Home from "./components/HomeComponent";
@@ -12,9 +14,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
+
+  HomePage = () => {
+    return (
+      <Home
+        dish={this.state.dishes.filter(dish => dish.featured)[0]}
+        promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+        leader={this.state.leaders.filter(leader => leader.featured)[0]}
+      />
+    );
+  };
 
   render() {
     return (
@@ -27,7 +42,7 @@ class App extends Component {
               component={() => <Menu dishes={this.state.dishes} />}
             />
 
-            <Route path="/home" component={() => <Home />} />
+            <Route path="/home" component={this.HomePage} />
             <Route path="/contactus" component={() => <Contact />} />
             <Redirect to="/home" />
           </Switch>
